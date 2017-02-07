5e519587850fb2ec2c2869c6d85487398add9e4d
yauheni5 <yauheni.asheichyk@gmail.com>
2017-02-08 00:17:11 +0300

    firstcommit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    browserSync = require('browser-sync');
+
+
+gulp.task('less', function() {
+	return gulp.src('app/styles/less/main.less')
+	.pipe(less())
+	.pipe(gulp.dest('app/styles/css'))
+	.pipe(browserSync.reload( {
+		stream:true
+	}))
+});
+
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+			baseDir: ''
+		},
+		notify:false
+	});
+});
+
+gulp.task('watch', ['browserSync', 'less'], function() {
+	 gulp.watch('app/styles/less/*.less', ['less']);
+});
+
