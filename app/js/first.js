8a94fb578d2bd655f06bb51d7ae228ad93f8cd05
yauheni5 <yauheni.asheichyk@gmail.com>
2017-02-17 01:39:29 +0300

    add JS for search

--- app/js/first.js
+++ app/js/first.js
@@ -1,3 +1,16 @@
-var hello = 'Я - JavaScript';
+var hello = 'Мы рады видеть Вас!';
+var empty = 'Пожалуйста, введите значение для поиска'
+
+onload = function() {
 	alert(hello);
+	document.getElementById('buttonSearch').onclick = function() {
+		if (document.getElementById('inputSearch').value == '') {
+			alert(empty);
+		}
+		else {
+	  	alert(document.getElementById('inputSearch').value);
+		}
+	}
+}
+
 
