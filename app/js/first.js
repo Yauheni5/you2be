2bad82eabcdd3d4d84e1c47c9aa72d5b3bbc61fb
Yauheni5 <yauheni.asheichyk@gmail.com>
2017-02-17 12:23:30 +0300

    Update first.js

--- app/js/first.js
+++ app/js/first.js
@@ -1,14 +1,15 @@
 var hello = 'Мы рады видеть Вас!';
-var empty = 'Пожалуйста, введите значение для поиска'
+var empty = 'Пожалуйста, введите значение для поиска';
+var inpSearch = document.getElementById('inputSearch').value;
 
 window.onload = function() {
 	alert(hello);
 	document.getElementById('buttonSearch').onclick = function() {
-		if (document.getElementById('inputSearch').value == '') {
+		if (inpSearch == '') {
 			alert(empty);
 		}
 		else {
-	  	alert(document.getElementById('inputSearch').value);
+	  	alert(inpSearch);
 		}
 	}
 }
