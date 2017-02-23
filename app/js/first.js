99cbe46e13ce4a837d5146be1708f6c3744dd12f
yauheni5 <yauheni.asheichyk@gmail.com>
2017-02-23 23:21:22 +0300

    remove inline style

--- app/js/first.js
+++ app/js/first.js
@@ -1,79 +1,67 @@
-//Desktop version
-
-var hello = 'Мы рады видеть Вас!';
-var empty = 'Пожалуйста, введите значение для поиска';
-var hello_mobile = 'Это мобильная версия'
-
 window.onload = function() {
-	if (window.matchMedia("screen and (max-width: 768px)").matches) {
-    alert(hello_mobile);
-  }
-  else {
-  	alert(hello);
-  }
-	function searchFunction() {
-		var inpSearch = document.getElementById('inputSearch').value;
-		if (inpSearch == '')  {
-			alert(empty);
-		}
-		else {
-	  	alert(inpSearch);
-		}
-	}
-	document.getElementById('buttonSearch').onclick = function () {
-		searchFunction();
-	}
+	var hello = 'Мы рады видеть Вас!';
+	var empty = 'Пожалуйста, введите значение для поиска';
+	var helloMobile = 'Это мобильная версия';
+	var widthScreenMax768 = window.matchMedia("screen and (max-width: 768px)").matches;
 
-	document.getElementById('inputSearch').onkeypress = function (){
-		if (event.keyCode == 13) searchFunction();
-	}
-	var divInputMobile = document.getElementById('divInputMobile');
-	var secondOpacityHeader = document.getElementById('secondLineHeaderMobile');
-	function openbox() {
-		if (divInputMobile.style.display == 'none') {
-			divInputMobile.style.display = 'flex';
-		}
-		else	{
-			divInputMobile.style.display = 'flex';
-		}
-	}
-	function searchFunctionMobile() {
-		if (divInputMobile.style.display == 'flex') {
-			var inpSearchMobile = document.getElementById('searchMobileInput').value;
+	if (widthScreenMax768) {
+    alert(helloMobile);
+    var divInputMobileVar = document.getElementById('divInputMobile');
+
+    function searchFunctionMobile() {
+    	var inpSearchMobile = document.getElementById('searchMobileInput').value;
 			if (inpSearchMobile == '')  {
 				alert(empty);
 			}
 			else {
 	  		alert(inpSearchMobile.split(' ').reverse().join(' '));
 			}
+    }
+    function openbox() {
+    	if (divInputMobileVar.classList.contains('hide')) {
+    		divInputMobileVar.classList.remove('hide');
+    		divInputMobileVar.classList.add('disp-flex');
+    		logoNameMobile.classList.remove('logo-name-mobile');
+    		logoNameMobile.classList.add('hide');
+				menuMobile.classList.add('hide');
+				secondLineHeaderMobile.classList.add('background-opacity');
+    	}
+    	else {
+    		searchFunctionMobile();
+			}
 		}
-		else {
-			return openbox();
+		function closeBox() {
+			divInputMobileVar.classList.add('hide');
+    	divInputMobileVar.classList.remove('disp-flex');
+    	logoNameMobile.classList.add('logo-name-mobile');
+    	logoNameMobile.classList.remove('hide');
+			menuMobile.classList.remove('hide');
+			secondLineHeaderMobile.classList.remove('background-opacity');
 		}
-	}
-	function hideNameLogoAndMenu() {
-		if (divInputMobile.style.display == 'flex') {
-			logoNameMobile.style.display = 'none';
-			menuMobile.style.display = 'none';
-			secondOpacityHeader.style.background = 'rgba(0,0,0,0.65)';
+
+    document.getElementById('buttonSearchMobile').onclick = function () {
+    	openbox();
+    }
+		document.getElementById('secondLineHeaderMobile').onclick = function() {
+			closeBox();
 		}
-		else	{
+		document.getElementById('searchMobileInput').onkeypress = function (){
+		if (event.keyCode == 13) searchFunctionMobile();}
+  }
+	else {
+		alert(hello);
+		function searchFunction() {
+			var inpSearch = document.getElementById('inputSearch').value;
+			if (inpSearch == '')  {
+				alert(empty);
+			}
+			else {
+		  	alert(inpSearch);
+			}
 		}
-	}
-	document.getElementById('buttonSearchMobile').onclick = function () {
-		openbox();
-	}
-	document.getElementById('buttonSearchMobile').onclick = function () {
-		searchFunctionMobile();
-		hideNameLogoAndMenu();
-	}
-	document.getElementById('secondLineHeaderMobile').onclick = function () {
-		divInputMobile.style.display = 'none';
-		logoNameMobile.style.display = 'flex';
-		menuMobile.style.display = 'flex';
-		secondOpacityHeader.style.background = '#e62117';
-	}
-	document.getElementById('searchMobileInput').onkeypress = function (){
-		if (event.keyCode == 13) searchFunctionMobile();
+		document.getElementById('buttonSearch').onclick = function() {
+			searchFunction();}
+		document.getElementById('inputSearch').onkeypress = function (){
+		if (event.keyCode == 13) searchFunction();}
 	}
 }
