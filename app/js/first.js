28d35734859b36ef6b283890d7be89258da6ad2e
yauheni5 <yauheni.asheichyk@gmail.com>
2017-02-24 20:55:48 +0300

    bugFix resizing

--- app/js/first.js
+++ app/js/first.js
@@ -6,62 +6,66 @@ window.onload = function() {
 
 	if (widthScreenMax768) {
     alert(helloMobile);
-    var divInputMobileVar = document.getElementById('divInputMobile');
-
-    function searchFunctionMobile() {
-    	var inpSearchMobile = document.getElementById('searchMobileInput').value;
-			if (inpSearchMobile == '')  {
-				alert(empty);
-			}
-			else {
-	  		alert(inpSearchMobile.split(' ').reverse().join(' '));
-			}
-    }
-    function openbox() {
-    	if (divInputMobileVar.classList.contains('hide')) {
-    		divInputMobileVar.classList.remove('hide');
-    		divInputMobileVar.classList.add('disp-flex');
-    		logoNameMobile.classList.remove('logo-name-mobile');
-    		logoNameMobile.classList.add('hide');
-				menuMobile.classList.add('hide');
-				secondLineHeaderMobile.classList.add('background-opacity');
-    	}
-    	else {
-    		searchFunctionMobile();
-			}
-		}
-		function closeBox() {
-			divInputMobileVar.classList.add('hide');
-    	divInputMobileVar.classList.remove('disp-flex');
-    	logoNameMobile.classList.add('logo-name-mobile');
-    	logoNameMobile.classList.remove('hide');
-			menuMobile.classList.remove('hide');
-			secondLineHeaderMobile.classList.remove('background-opacity');
-		}
-
-    document.getElementById('buttonSearchMobile').onclick = function () {
-    	openbox();
-    }
-		document.getElementById('secondLineHeaderMobile').onclick = function() {
-			closeBox();
-		}
-		document.getElementById('searchMobileInput').onkeypress = function (){
-		if (event.keyCode == 13) searchFunctionMobile();}
   }
 	else {
 		alert(hello);
-		function searchFunction() {
-			var inpSearch = document.getElementById('inputSearch').value;
-			if (inpSearch == '')  {
-				alert(empty);
-			}
-			else {
-		  	alert(inpSearch);
-			}
-		}
-		document.getElementById('buttonSearch').onclick = function() {
-			searchFunction();}
-		document.getElementById('inputSearch').onkeypress = function (){
-		if (event.keyCode == 13) searchFunction();}
 	}
+
+	//ФУНКЦИИ и переменные для Десктоп
+	function searchFunction() {
+		var inpSearch = document.getElementById('inputSearch').value;
+		if (inpSearch == '')  {
+			alert(empty);
+		}
+		else {
+		  alert(inpSearch);
+		}
+	}
+	//ФУНКЦИИ и переменные для Мобильных устройств
+	var divInputMobileVar = document.getElementById('divInputMobile');
+  function searchFunctionMobile() {
+    var inpSearchMobile = document.getElementById('searchMobileInput').value;
+		if (inpSearchMobile == '')  {
+			alert(empty);
+		}
+		else {
+	  	alert(inpSearchMobile.split(' ').reverse().join(' '));
+		}
+  }
+  function openbox() {
+  	if (divInputMobileVar.classList.contains('hide')) {
+	    divInputMobileVar.classList.remove('hide');
+	    divInputMobileVar.classList.add('disp-flex');
+	    logoNameMobile.classList.remove('logo-name-mobile');
+	    logoNameMobile.classList.add('hide');
+			menuMobile.classList.add('hide');
+			secondLineHeaderMobile.classList.add('background-opacity');
+   	}
+    else {
+    	searchFunctionMobile();
+		}
+	}
+	function closeBox() {
+		divInputMobileVar.classList.add('hide');
+    divInputMobileVar.classList.remove('disp-flex');
+    logoNameMobile.classList.add('logo-name-mobile');
+    logoNameMobile.classList.remove('hide');
+		menuMobile.classList.remove('hide');
+		secondLineHeaderMobile.classList.remove('background-opacity');
+	}
+	//События
+	document.getElementById('buttonSearch').onclick = function() {
+		searchFunction();}
+	document.getElementById('inputSearch').onkeypress = function (){
+	  if (event.keyCode == 13) searchFunction();}
+  document.getElementById('buttonSearchMobile').onclick = function () {
+    openbox();
+    console.log(this);
+  }
+	document.getElementById('secondLineHeaderMobile').onclick = function() {
+		closeBox();
+		console.log(this);
+	}
+	document.getElementById('searchMobileInput').onkeypress = function (){
+		if (event.keyCode == 13) searchFunctionMobile();}
 }
