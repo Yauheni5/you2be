4b1d3ba7261b1c05265f6e2ec20c80fe12f9b5f2
yauheni5 <yauheni5@github.com>
2017-03-07 21:36:03 +0300

    push

--- app/js/first.js
+++ app/js/first.js
@@ -1,11 +1,22 @@
 var minWidthDesktop = 769;
-var hello = 'Мы рады видеть Вас!';
-var empty = 'Пожалуйста, введите значение для поиска';
-var helloMobile = 'Это мобильная версия';
-var divVar = document.getElementsByTagName('div');
+var empty = "Введите запрос!"
 var inputSearchVar = document.getElementById('inputSearch');
 
 widthScreen();
+
+
+function searchFunction() {
+	var inpSearch = document.getElementById('inputSearch').value;
+	if (inpSearch == '') alert(empty);
+	else alert(inpSearch);
+}
+
+function openBox() {
+	if (document.getElementsByClassName("responsive")[0].classList.contains('open-box')) {
+		searchFunction();
+	} else document.getElementsByClassName("responsive")[0].classList.add('open-box');
+	return
+}
 
 function widthScreen() {
 	var screenWidthVar = window.outerWidth;
@@ -18,6 +29,7 @@ function widthScreen() {
 		elResponsiveClass.classList.remove('mobile');
 		elOnlyDesktopClass.classList.remove('hide');
 		elOnlyMobileClass.classList.add('hide');
+		closeBox();
 		document.getElementById('buttonSearch').onclick = function() {
 			searchFunction();
 		}
@@ -33,58 +45,19 @@ function widthScreen() {
 	}
 }
 
-//ФУНКЦИИ и переменные для Десктоп
-
-//ФУНКЦИИ и переменные для Мобильных устройств
-//*********************************************************************************
-
-function searchFunction() {
-
-			var inpSearch = document.getElementById('inputSearch').value;
-
-			if (inpSearch == '') alert(empty);
-			else alert(inpSearch);
-		}
-
-function openBox() {
-	if (document.getElementsByClassName("responsive")[0].classList.contains('open-box')) {
-		searchFunction();
-	} else document.getElementsByClassName("responsive")[0].classList.add('open-box');
-	return
-}
-
 function closeBox() {
 	document.getElementsByClassName("responsive")[0].classList.remove('open-box');
 }
-//Отслеживание изменения размера окна браузера и если это необходимо меняет Mobile-version на Desktop и наоборот
-	//*********************************************************************************
-	window.addEventListener("resize",	widthScreen);
 
-	window.onload = function() {
-	//Общие функции и переменные
-//*********************************************************************************
-	//Поиск при клике на кнопку поиск(лупа)	десктопная версия
-	//*********************************************************************************
+window.addEventListener("resize",	widthScreen);
 
-	//Поиск при нажатии на Enter(клавиатура)
-	//*********************************************************************************
+window.onload = function() {
+
 	document.getElementById('inputSearch').onkeypress = function (){
-		if (event.keyCode == 13) searchFunction();
+	if (event.keyCode == 13) searchFunction();
 	}
-	//Открытие поля ввода и Поиск при клике на кнопку поиск(лупа)	мобильная версия
-	//*********************************************************************************
-//  document.getElementById('buttonSearch').onclick = function () {
-//    openbox();
-//  }
-	//скрытие поля ввода при клике в неактивном месте
-	//*********************************************************************************
+
 	document.getElementById('secondLineHeaderMobile').onclick = function() {
 		closeBox();
 	}
-	//Поиск при нажатии на Enter(клавиатура) мобильная версия
-	//*********************************************************************************
-//	document.getElementById('searchMobileInput').onkeypress = function (){
-//		if (event.keyCode == 13) searchFunctionMobile();
-//	}
-
 }
